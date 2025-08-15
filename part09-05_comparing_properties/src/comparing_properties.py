e6583e3c02a130148574a6e98a7866fbafaf7520
wartyfish <jamiefendt96@gmail.com>
2025-08-15 13:38:13 +0100

    0905, PC

---
+++ part09-05_comparing_properties/src/comparing_properties.py
@@ -0,0 +1,25 @@
+class RealProperty:
+    def __init__(self, rooms: int , square_metres: int , price_per_sqm:int):
+        self.rooms = rooms
+        self.square_metres = square_metres
+        self.price_per_sqm = price_per_sqm
+
+    def bigger(self, compared_to):
+        is_bigger = True if self.square_metres > compared_to.square_metres else False
+        return is_bigger
+    
+    def price_difference(self, compared_to):
+        return abs(self.square_metres*self.price_per_sqm - compared_to.square_metres*compared_to.price_per_sqm)
+    
+    def more_expensive(self, compared_to):
+        diff = self.square_metres*self.price_per_sqm - compared_to.square_metres*compared_to.price_per_sqm
+        result = True if diff > 0 else False
+        return result
+    
+if __name__ == "__main__":
+    central_studio = RealProperty(1, 16, 5500)
+    downtown_two_bedroom = RealProperty(2, 38, 4200)
+    suburbs_three_bedroom = RealProperty(3, 78, 2500)
+
+    print(central_studio.more_expensive(downtown_two_bedroom))
+    print(suburbs_three_bedroom.more_expensive(downtown_two_bedroom))
