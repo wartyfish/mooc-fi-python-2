35c8b30e1103949a0713c87853aeb0fe74154ad6
wartyfish <jamiefendt96@gmail.com>
2025-08-15 13:06:31 +0100

    Update up to 0904 from PC

---
+++ part09-04_lunchcard_and_paymentterminal/src/lunchcard_and_paymentterminal.py
@@ -0,0 +1,78 @@
+class LunchCard:
+    def __init__(self, balance: float):
+        self.balance = balance
+
+    def deposit_money(self, amount: float):
+        self.balance += amount
+
+    def subtract_from_balance(self, amount: float):
+        if self.balance - amount >= 0:
+            self.balance -= amount
+            return True
+        return False
+
+class PaymentTerminal:
+    def __init__(self):
+        self.funds = 1000
+        self.lunches = 0
+        self.specials = 0
+
+    def eat_lunch(self, payment: float):
+        cost = 2.5
+        if payment >= cost:
+            self.funds += cost
+            self.lunches += 1
+            return payment - cost
+        else:
+            return payment
+
+    def eat_special(self, payment: float):
+        cost = 4.3
+        if payment >= cost:
+            self.funds += cost
+            self.specials += 1
+            return payment - cost
+        else:
+            return payment
+
+    def eat_lunch_lunchcard(self, card: LunchCard):
+        cost = 2.5
+        if card.balance >= cost:
+            card.balance -= cost
+            self.lunches += 1
+            return True
+        else:
+            return False
+
+    def eat_special_lunchcard(self, card: LunchCard):
+        cost = 4.3
+        if card.balance >= cost:
+            card.balance -= cost
+            self.specials += 1
+            return True
+        else:
+            return False
+
+    def deposit_money_on_card(self, card: LunchCard, amount: float):
+        card.balance += amount
+        self.funds += amount
+
+if __name__ == "__main__":
+    exactum = PaymentTerminal()
+
+    card = LunchCard(2)
+    print(f"Card balance is {card.balance} euros")
+
+    result = exactum.eat_special_lunchcard(card)
+    print("Payment successful:", result)
+
+    exactum.deposit_money_on_card(card, 100)
+    print(f"Card balance is {card.balance} euros")
+
+    result = exactum.eat_special_lunchcard(card)
+    print("Payment successful:", result)
+    print(f"Card balance is {card.balance} euros")
+
+    print("Funds available at the terminal:", exactum.funds)
+    print("Regular lunches sold:", exactum.lunches)
+    print("Special lunches sold:", exactum.specials)
