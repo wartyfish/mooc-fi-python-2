49c58f41ee76ee40a87f8114ff83f962a573f1e5
wartyfish <jamiefendt96@gmail.com>
2025-09-05 12:33:08 +0100

    PC. Functions within functions: custom sorts of lists, dictionaries and objects.

---
+++ part12-01_remaining_stock/src/remaining_stock.py
@@ -0,0 +1,11 @@
+def sort_by_remaining_stock(items: list):
+    def order_by_remaining_stock(item: tuple):
+        return item[2]
+    
+    return sorted(items, key=order_by_remaining_stock)
+
+if __name__ == "__main__":
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22)]
+
+    for product in sort_by_remaining_stock(products):
+        print(f"{product[0]} {product[2]} pcs")
